61543f73f89061be466c620c41c3a9f72a7ca6ad
marcel <filej1025@gmail.com>
2020-10-22 12:41:10 +0200

    2nd commit -  function and struct

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <array>
+#include <cstdlib>
 #include <vector>
 
 // Define a struct
@@ -10,18 +10,39 @@ struct Point_2D {
 
 };
 
+// INPUT ---> {????} ---> OUTPUT
+//      {initialize struct}
+
+Point_2D create_point() {
+    Point_2D tmp;
+    tmp.x = rand() % 10;
+    tmp.y = rand() % 10;
+
+    return tmp;
+}
+
+// Pass by refference ( don't forget &)
+void initialize_in_place(Point_2D & point) {
+    point.x = rand() % 10;
+    point.y = rand() % 10;
+
+}
 
 int main() { 
     // Declare a variable with our struct and initialize its members
-    Point_2D my_point {2,3};
-
-    // It is possible to initialize its members also this way: 
-    // my_point.x = 2;
-    // my_point.y = 3;
+    Point_2D my_point = create_point(); 
 
     // Print its contents
     std::cout << "Value of x: " << my_point.x << std::endl;
     std::cout << "Value of y: " << my_point.y << std::endl;
+
+    // Declare a variable with our struct and initialize its members
+    Point_2D empty_point;
+    initialize_in_place(empty_point);
+
+    // Print its contents
+    std::cout << "Value of x: " << empty_point.x << std::endl;
+    std::cout << "Value of y: " << empty_point.y << std::endl;
 
 
 
